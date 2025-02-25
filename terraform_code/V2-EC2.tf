436fceeb4a71b1ddd05d96dfd4938c6bd5c34e67
brandoff <whiteivyson@gmail.com>
2025-02-25 12:14:41 -0600

    adding V2

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -3,11 +3,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "demo-server" {
-  ami           = "ami-05b10e08d247fb927"
-  instance_type = "t2.micro"
-  key_name      = "ddp"
-  security_groups = ["demo_sg"]
+resource "aws_instance" "demo_server" {
+  ami             = "ami-05b10e08d247fb927"
+  instance_type   = "t2.micro"
+  key_name        = "ddp"
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
 }
 
 resource "aws_security_group" "demo_sg" {
@@ -19,7 +19,7 @@ resource "aws_security_group" "demo_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allows SSH from anywhere (Change for security)
+    cidr_blocks = ["0.0.0.0/0"] # Allows SSH from anywhere (Change for security)
   }
 
   # Outbound Rules
@@ -27,7 +27,7 @@ resource "aws_security_group" "demo_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # Allows all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"] # Allows all outbound traffic
   }
 
   tags = {
