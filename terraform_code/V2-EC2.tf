27ec396249118649452e19b2022b38623d971ee5
brandoff <whiteivyson@gmail.com>
2025-02-25 11:32:21 -0600

    initial commit

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -1,36 +1,36 @@
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
-    security_groups = [ "demo-sg" ]
+  ami           = "ami-05b10e08d247fb927"
+  instance_type = "t2.micro"
+  key_name      = "ddp"
+  security_groups = ["demo_sg"]
 }
 
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "SSH Access"
-  
-  ingress {
-    description      = "Shh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
+resource "aws_security_group" "demo_sg" {
+  name        = "demo_sg"
+  description = "Allow inbound SSH traffic"
 
+  # Inbound Rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allows SSH from anywhere (Change for security)
+  }
+
+  # Outbound Rules
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allows all outbound traffic
   }
 
   tags = {
-    Name = "ssh-prot"
-
+    Name = "ssh-port"
   }
 }
